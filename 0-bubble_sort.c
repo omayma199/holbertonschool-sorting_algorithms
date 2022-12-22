b699c679cbba2d0f8de48e5e3cfeb77a43876702
omayma199 <5752@holbertonstudents.com>
2022-12-22 02:13:06 +0100

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,24 +7,23 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j,  tmp;
+	size_t i, j, tmp;
 
-    if (size <= 1)
-        {
-            return;
-        }
-    for (i = 0; i < size  -  1; i++)
-    {
-        for (j = 0; j <= size - 2; j++)
-            {
-                if (array[j] > array[j + 1])
-                {
-                    tmp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = tmp;
-                print_array(array, size);
-                }
-            }
-    }
+	if (size <= 1)
+	{
+		return;
+	}
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j <= size - 2; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
 }
-
