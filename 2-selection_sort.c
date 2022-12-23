b222614d951c07b6c63f7a939552589ff4de85b9
omayma199 <5752@holbertonstudents.com>
2022-12-23 21:24:40 +0100

    Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,30 +1,33 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - sorts a doubly linked list of integers in ascending
+ * selection_sort - sorts an array of integers in ascending order
  * order using an insertion sort algorithm
- * @list: doubly linked list of integers to be sorted
- */ 
- void selection_sort(int *array, size_t size)
- {
-    size_t i, j, min_idx;
+ * @array: list of integers
+ * @size: number of elements
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_idx, t;
 
 	if (size <= 1)
 	{
 		return;
 	}
- 
-    // One by one move boundary of unsorted subarray
-    for (i = 0; i < size - 1; i++)
-    {
-        // Find the minimum element in unsorted array
-        min_idx = i;
-        for (j = i+1; j < size; j++)
-          if (array[j] < array[min_idx])
-            min_idx = j;
- 
-        // Swap the found minimum element with the first element
-           if(min_idx != i)
-            swap(&array[min_idx], &array[i]);
-    }
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+			min_idx = j;
+		}
+		if (min_idx != i)
+			{
+				t = array[i];
+				array[i] = array[min_idx];
+				array[min_idx] = t;
+			}
+	}
 }
